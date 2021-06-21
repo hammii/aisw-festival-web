068f5608470b5b0c41b4f0fcc799ed6367eed521
장하영 <hyj9829@naver.com>
2021-06-21 14:23:26 +0900

    feat: CSVtoArray

--- List/list.js
+++ List/list.js
@@ -33,3 +33,61 @@ function loaded() {
                     </li>`); //ul_list안쪽에 li추가
 // }
 }
+
+$(function () {
+    var fileName = "assets/test.csv";
+
+    $.ajax({
+        url: fileName,
+        dataType: 'text',
+        success: function (data) {
+            var allRow = data;
+            var allData = "";
+
+            for (var singleRow = 0; singleRow < allRow.length; singleRow++) {
+                allData += allRow[singleRow];
+                // var collapse = allRow[singleRow].split('\\n');
+
+                // for(var count = 0; count < allRow.length; count++) {
+                //     if(collapse[count] === " ")
+                //         console.log("ㅎㅇ");
+                //
+                //     textLine += collapse[count];
+                // }
+            }
+
+            var rowArr = allData.split('\n');
+            var textArr = new Array(rowArr.length);
+
+            for (var num = 0; num < rowArr.length; num++) {
+                textArr[num] = CSVtoArray(rowArr[num]);
+            }
+            console.log(textArr);
+
+            $("#textArea").append(allData);
+
+            // processData(data)
+        }
+    });
+
+    function CSVtoArray(text) {
+        var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+        var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+        //Return NULL if input string is not well formed CSV string.
+        if (!re_valid.test(text)) return null;
+        var a = []; //Initialize array to receive values.
+        text.replace(re_value, //"Walk" the string using replace with callback.
+            function (m0, m1, m2, m3) {
+                //Remove backslash from \' in single quoted values.
+                if (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+                //Remove backslash from \" in double quoted values.
+                else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+                else if (m3 !== undefined) a.push(m3);
+                return ''; //Return empty string.
+            });
+        //Handle special case of empty last value.
+        if (/,\s*$/.test(text)) a.push('');
+        return a;
+    }
+
+});
